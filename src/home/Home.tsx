4dcbf3256fc4b1d910ed098d28c247c246fa8fdb
BlueRocks <wfkbitol@126.com>
2025-09-28 20:45:47 +0800

    完成首页的头部页面

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,33 +1,32 @@
 import styles from "./Home.module.css";
-import maple from "../assets/maple.svg";
+import icons from "../assets/icons.svg";
 
 function Home() {
     return (
-        <div className={styles["main"]}>
-            <div className={styles["nav-container"]}>
-                <div className={styles["nav1"]}>
-                    <img className={styles["maple"]} src={maple}/>
-                    <div className={styles["title"]}>Red Maple</div>
-                    <div className={styles["about"]}>About</div>
-                </div>
-                <div className={styles["nav2"]}>
-                    <div className={styles["link"]}>WPF</div>
-                    <div className={styles["link"]}>Web</div>
-                    <div className={styles["link"]}>C#</div>
-                    <div className={styles["link"]}>Javascript</div>
-                </div>
+        <section className={styles["header"]}>
+            <div className={styles["title"]}>
+                红枫树
             </div>
-            <div className={styles["theme"]}>
-                <img src="https://z.wiki/u/otxhGS" />
-                <div className={styles["motto"]}>
-                    <div className={styles["cover"]}/>
-                    <div className={styles["content"]}>
-                        You have to trust in something, your gut, destiny, life, karma, whatever.
-                        <div className={styles["author"]}>- Steve Jobs</div>
-                    </div>
-                </div>
+            <div className={styles["bar"]} />
+            <div className={styles["slogan"]}>
+                You have to trust in something, your gut, destiny, life, karma, whatever.
             </div>
-        </div>
+
+
+            <nav className={styles["nav"]}>
+                <button>C#</button>
+                <button>WPF</button>
+                <button>JS</button>
+                <button>杂七杂八</button>
+            </nav>
+
+            <div className={styles["scroll-indicator-container"]}>
+                <svg viewBox="0 0 640 640" className={styles["scroll-indicator"]}>
+                    <use href={`${icons}#arrow-down`} />
+                </svg>
+            </div>
+        </section>
+
     )
 }
 
